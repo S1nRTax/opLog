1c2dcac259d16aa3ee52a22fc053f7f026b27024
S1nRTax <badreddine.asaad4@gmail.com>
2025-09-08 20:51:28 +0100

    defined log levels

--- include/opLog/log_level.h
+++ include/opLog/log_level.h
@@ -2,4 +2,15 @@
 #ifndef LOG_LEVEL_H
 #define LOG_LEVEL_H
 
+
+enum class LogLevel {
+    TRACE, // highest priority
+    DEBUG,
+    INFO,
+    WARN,
+    ERROR,
+    FATAL
+};
+
+
 #endif //LOG_LEVEL_H
